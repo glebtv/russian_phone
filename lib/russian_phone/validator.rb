2114ca0ba090b6dd3c5d3ff84c2f8d50ffb9fe06
glebtv <glebtv@gmail.com>
2013-02-05 21:29:54 +0400

    Update lib/russian_phone/validator.rb

--- lib/russian_phone/validator.rb
+++ lib/russian_phone/validator.rb
@@ -4,7 +4,9 @@ module RussianPhone
   class Validator < ActiveModel::Validator
     def validate(record)
       options[:fields].each do |field|
-        record.errors[field] << 'Неверный телефонный номер' unless record.send(field).valid? && record.send(field).city_allowed?
+        unless record.send(field).blank?
+          record.errors[field] << 'Неверный телефонный номер' unless record.send(field).valid? && record.send(field).city_allowed?
+        end
       end
     end
   end
