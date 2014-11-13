494e437cdc52e4b4118d65c0b59810be2cc2e2ce
Gleb <glebtv@gmail.com>
2014-11-13 19:46:16 +0300

    try fix AR

--- lib/russian_phone/rails_admin.rb
+++ lib/russian_phone/rails_admin.rb
@@ -1,23 +1,25 @@
-require 'rails_admin/adapters/mongoid'
 begin
-  require 'rails_admin/adapters/mongoid/property'
-rescue Exception => e 
-end
-
-module RailsAdmin
-  module Adapters
-    module Mongoid
-      class Property
-        alias_method :type_without_russian_phone, :type
-        def type
-          if property.type.class.name == "RussianPhone::Field"
-            :string
-          else
-            type_without_russian_phone
+  require 'rails_admin/adapters/mongoid'
+  begin
+    require 'rails_admin/adapters/mongoid/property'
+  rescue Exception => e 
+  end
+  
+  module RailsAdmin
+    module Adapters
+      module Mongoid
+        class Property
+          alias_method :type_without_russian_phone, :type
+          def type
+            if property.type.class.name == "RussianPhone::Field"
+              :string
+            else
+              type_without_russian_phone
+            end
           end
         end
       end
     end
   end
+rescue Exception => e 
 end
-
